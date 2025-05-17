6498565759cce0efeabac47e803b8c9a29715a13
Sanskarpurohit <Sanskar.Purohit@cognizant.com>
2025-05-17 16:05:02 +0530

    fashionblog

--- blocks/centerdiv/centerdiv.js
+++ blocks/centerdiv/centerdiv.js
@@ -4,9 +4,9 @@ const headingBox = contentWrapper.children[0];
 const subheadingBox = contentWrapper.children[1];
 headingBox.classList.add('heading-box');
 subheadingBox.classList.add('subheading-box');
-const h6 = subheadingBox.querySelector('h6');
+const h6 = document.getElementById("welcome-to-the-blog-of-janes-world")
 h6.classList.add('blog-line');
 h6.innerHTML = h6.innerHTML.replace(
- 'Jane’s world',
+ "Jane's world",
  '<span class="blog-owner">Jane’s world</span>'
 );   
---
+++ blocks/fashionblog/fashionblog.js
@@ -0,0 +1,12 @@
+const fashionblog = document.querySelector('.fashionblog');
+const imageContainer = fashionblog.children[0];
+imageContainer.classList.add('image-container');
+const overlayContainer = fashionblog.children[1];
+overlayContainer.classList.add('overlay-text');
+const [janesDiv, fashionBlogDiv, subscribeDiv] = overlayContainer.children;
+janesDiv.querySelector('h1').id = 'janes';
+janesDiv.classList.add('janes-container');
+fashionBlogDiv.querySelector('h1').id = 'fashion-blog';
+fashionBlogDiv.classList.add('fashion-blog-container');
+subscribeDiv.querySelector('h6').id = 'subscribe';
+subscribeDiv.classList.add('subscribe-container');
