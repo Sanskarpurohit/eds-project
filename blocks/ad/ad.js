3e7ad1edd440c908b2c3efc9f1567f077d0bb6e9
Sanskarpurohit <Sanskar.Purohit@cognizant.com>
2025-05-17 21:16:25 +0530

    Advertise block added

---
+++ blocks/ad/ad.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.ad').forEach(ad => {
+    const titleWrapper = ad.children[0];
+    const titleInner = titleWrapper?.children[0];
+    if (titleInner) titleInner.classList.add('ad-title-block');
+    const imgWrapper = ad.children[1];
+    const imageInner = imgWrapper?.children[0];
+    if (imageInner) imageInner.classList.add('ad-image');
+   });
